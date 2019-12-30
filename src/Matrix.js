61f78cee4b5532d4b59fcb9ea5a8a14d61a7147b
markpnoi26 <markpnoi26@gmail.com>
2019-12-29 23:38:16 -0800

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 
 export default class Matrix extends Component {
-  
+
+  // this.props.values ===> array of arrays
+  // this.props.values = [
+  //                       [a, b, c],
+  //                       [a, b, c]
+  //                     ]
+
+
   genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    // vals = ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F']
+    vals.map(val => <div className="cell"></div>)
+    // replace me and render a cell component instead!
   )
-  
+
   genMatrix = () => (
     this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   )
-  
+
   render() {
     return (
       <div id="matrix">
@@ -17,5 +26,5 @@ export default class Matrix extends Component {
       </div>
     )
   }
-  
+
 }
