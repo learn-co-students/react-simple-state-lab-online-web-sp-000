c9c89a0f2ba5b00fcb1b0927891749e3fbc9bbb1
github username <github email address>
2020-05-22 19:45:40 -0600

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+  constructor() {
+    super()
+    console.log(this)
+    this.state = {
+      color: this.props.value
+    }
+  }
+
+  render() {
+    return (
+      <div onClick={() => this.setState({color: '#333'})} className="cell" style={{backgroundColor: this.state.color}}>
+      </div>
+    )
+  }
+
+}
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import Cell from './Cell'
+
+//! Once you have made your Cell component, replace the return value in 
+//! genRow's map to: <Cell value={val} />. 
+//! Here we are specifying our Cell component should have a value prop.
 
 export default class Matrix extends Component {
   
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    //console.log(vals)
+  return vals.map(val => {<Cell value={val} />} ) // replace me and render a cell component instead!
   }
-  
+  // val => <div className="cell"></div>
   genMatrix = () => {
+    //console.log(this.props.values)
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   }
   
@@ -18,4 +25,19 @@ export default class Matrix extends Component {
     )
   }
   
+}
+
+Matrix.defaultProps = {
+  values: [
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+  ]
 }
