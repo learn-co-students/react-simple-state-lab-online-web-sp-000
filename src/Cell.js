a859607ba31c7321acebeb0819dae92a5cab2fa1
Soner Mezgitci <mezgitci9@gmail.com>
2020-07-18 00:24:14 -0400

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Cells extends component {
+constructor() {
+    super()
+    this.state = {
+       color:value 
+    }
+}
+
+handleClick = () => {
+{this.value}
+}
+
+
+
+
+render() {
+  return (
+    <div onClick = {this.handleClick} className="cell" style={this.state}>
+    
+    
+   
+    </div>
+    )
+
+    }
+}
