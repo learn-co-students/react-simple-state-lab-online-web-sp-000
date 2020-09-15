ed5cf968e3dfeffae33ca4ee472a19049cdc67e3
github username <github email address>
2020-09-15 14:08:18 -0700

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -4,11 +4,22 @@ import Cell from './cell';
 export default class Matrix extends Component {
   
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"><Cell value={val} /></div>) // replace me and render a cell component instead!
+    // console.log(vals)
+    // vals is one array with 10 colors
+
+    // cell is a component from cell.js, when the div is created it is adding the color to the div
+    // <Cell value={val} />
+
+    return vals.map(val => <div className="cell"> <Cell value={val}/> </div>) // replace me and render a cell component instead!
   }
   
-  genMatrix = () => {
-    return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
+  // genMatrix is an instance method
+  genMatrix = () => 
+  {
+    // console.log(this.props.values)
+    // this.props.value is an array of 10 arrays with 10 colors within each array
+    return this.props.values.map(rowVals => <div className="row"> {this.genRow(rowVals)} </div>)
+    
   }
   
   render() {
