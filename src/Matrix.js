4d6cdb0a6bce9e8e6efab7b83d88c48f4826720e
Joseph Perez <hello@josephdanielperez.com>
2021-01-27 14:33:12 -0800

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -7,6 +7,7 @@ export default class Matrix extends Component {
   }
   
   genMatrix = () => {
+    console.log(this.props)
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   }
   
@@ -18,4 +19,11 @@ export default class Matrix extends Component {
     )
   }
   
+}
+
+Matrix.defaultProps = {
+  values: (() => {
+    const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'];
+    return (new Array(10).fill(defRow));
+  })()
 }
