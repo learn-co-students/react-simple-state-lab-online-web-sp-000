e9825dde2115333d0228cfd953c1e92fbddf5e16
Austin Rhoads <austinrrhoads@ymail.com>
2021-04-21 22:33:32 -0500

    health is about accepting and dealing with reality on realitys terms

---
+++ src/Cell.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+
+export default class Cell extends React.Component{
+
+    constructor(props) {
+        super(props)
+     
+        this.state = {
+         
+          color: props.value
+        }
+
+    }
+
+toGrey = () => {
+  this.setState({
+    color: '#333'
+  })
+  
+
+}
+
+
+
+    render(){
+   
+
+      return(
+        
+        <div className="cell" style={{
+          backgroundColor: this.state.color
+        }} onClick={this.toGrey}>
+
+        </div>
+      )
+    }
+
+
+}
